126e35ce6965b2892fe80999a6b4397e6dbf9a50
Ezra-zed <xiaounili0@gmail.com>
2026-09-24 16:18:23 +0530

    model

---
+++ models/User.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name:{ type: String, required: true },
+    email:{ type: String, required: true, unique: true },
+    role:{type:string, default: 'user'},
+    createdAt:{ type: Date, default: Date.now },
+    
+});
+module.exports = mongoose.model('User', userSchema);
