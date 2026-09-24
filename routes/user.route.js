0c8e2005d1b690bf6ab2a5c56757080684722acd
Ezra-zed <xiaounili0@gmail.com>
2026-09-24 16:57:15 +0530

    route update

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,9 +1,10 @@
 const router = require ('express').Router();
+const userController = require('../controller/User.controller');
 
-router.get('/api/users',);
-router.post('/api/users',);
-router.get('/api/users/:id',);
-router.put('/api/users/:id',);
-router.delete('/api/users/:id',);
+router.get('/api/users', userController.getUsers);
+router.post('/api/users', userController.createUser);
+router.get('/api/users/:id', userController.getUserById);
+router.put('/api/users/:id', userController.updateUser);
+router.delete('/api/users/:id', userController.deleteUser);
 
 module.exports = router;
